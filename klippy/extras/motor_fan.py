d279e7558ab864571b4775561a81b4af7b296308
cruwaller <cruwaller@gmail.com>
2018-03-25 18:53:21 +0300

    motor_fan fixed

--- klippy/extras/motor_fan.py
+++ klippy/extras/motor_fan.py
@@ -18,13 +18,18 @@ class MotorFan:
                           self.toolhead.logger.getChild(
                               self.fan.name.replace(" ", "_"))
             self.toolhead.register_cb('motor', self.callback)
+            self.reactor = self.printer.get_reactor()
     def callback(self, event, eventtime):
         if event == 'off':
-            power = 0.
+            self.power = 0.
         else:
-            power = self.fan_speed
+            self.power = self.fan_speed
+        self.set_timer = self.reactor.register_timer(self.reactor_callback,
+                                                     self.reactor.NOW)
+    def reactor_callback(self, eventtime):
         print_time = self.mcu.estimated_print_time(eventtime) + PIN_MIN_TIME
-        self.fan.set_speed(print_time, power)
+        self.fan.set_speed(print_time, self.power)
+        return self.reactor.NEVER
 
 def load_config(config):
     return MotorFan(config)
