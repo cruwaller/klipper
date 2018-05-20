7b7792d30c977b3e605ca42bcf39ef61683c6282
Kevin O'Connor <kevin@koconnor.net>
2018-05-20 11:28:28 -0400

    probe: Move code from bed_tilt and delta_calibrate into ProbePointsHelper
    Move the common config reading and probe object lookup from the
    bed_tilt.py and delta_calibrate.py code into the ProbePointsHelper
    class.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>
    
    Conflicts:
    	klippy/extras/probe.py

--- klippy/extras/probe.py
+++ klippy/extras/probe.py
@@ -162,15 +162,10 @@ class ProbePointsHelper:
             self.probe = self.printer.lookup_object('probe')
             self.lift_speed = min(self.speed, self.probe.speed)
             self.probe_z_offset = self.probe.z_offset
-            if self.horizontal_move_z < self.probe_z_offset:
-                raise config.error("horizontal_move_z can't be less than probe's"
-                                   " z_offset in %s" % (config.get_name()))
         # Internal probing state
         self.results = []
         self.busy = False
         self.gcode = self.toolhead = None
-    def get_lift_speed(self):
-        return self.lift_speed
     def start_probe(self):
         # Begin probing
         self.toolhead = self.printer.lookup_object('toolhead')
