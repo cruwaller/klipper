707b1b7623be6500d64bec2cf421d22039694b45
cruwaller <cruwaller@gmail.com>
2018-06-15 14:52:21 +0300

    hostcmd: code cleanup + print to debug

--- klippy/modules/hostcmd.py
+++ klippy/modules/hostcmd.py
@@ -28,13 +28,12 @@ class Command(object):
         gcode.register_command(
             name, self.execute, when_not_ready=True,
             desc="Execute host command '%s'" % command)
-        self.logger.info("ALIAS %s : %s registered" % (name, command))
+        self.logger.debug("ALIAS %s : %s registered" % (name, command))
     def execute(self, params):
         self.logger.debug("command %s executed" % self.command)
         msg = os.popen(self.command).read()
         self.gcode.respond_info(msg)
 
 def load_module(printer, config):
-    if not config.has_section("hostcmd"):
-        return None
-    return HostCmd(printer, config.getsection("hostcmd"))
+    if config.has_section("hostcmd"):
+        HostCmd(printer, config.getsection("hostcmd"))
