da71ccfe95c80b09da4f7ca30e828189b764ec64
Kevin O'Connor <kevin@koconnor.net>
2019-04-01 22:20:21 -0400

    scripts: Add a new klippy-requirements.txt file for virtualenv installs
    Place all of the host software python requirements into a config file.
    This makes it easier to update the python requirements without having
    to update several installation scripts.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>
    
    Conflicts:
    	scripts/install-octopi.sh

--- scripts/install-octopi.sh
+++ scripts/install-octopi.sh
@@ -38,7 +38,7 @@ create_virtualenv()
     [ ! -d ${PYTHONDIR} ] && virtualenv ${PYTHONDIR}
 
     # Install/update dependencies
-    ${PYTHONDIR}/bin/pip install cffi==1.6.0 pyserial==3.2.1 greenlet==0.4.10
+    ${PYTHONDIR}/bin/pip install -r ${SRCDIR}/scripts/klippy-requirements.txt
     ${PYTHONDIR}/bin/pip install -r ${SRCDIR}/requirements.txt
     ${PYTHONDIR}/bin/pip install RPi.GPIO
 }
