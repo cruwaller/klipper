43bfd263d05ce071e80652e5d479ba5f911033f9
cruwaller <cruwaller@gmail.com>
2019-09-25 11:46:06 +0300

    driver: force section exist if driver defined for stepper

--- klippy/extras/driver/__init__.py
+++ klippy/extras/driver/__init__.py
@@ -20,9 +20,9 @@ DRV_MAPPING = {
     # Original versions
     'TMC2130_ORIG': tmc2130_tmp.TMC2130,
     'TMC2208': tmc2208.TMC2208,
+    'TMC2209': tmc2209.TMC2209,
     'TMC2224': tmc2208.TMC2208,
     'TMC2660': tmc2660.TMC2660,
-    'TMC2209': tmc2209.TMC2209,
     'TMC5160': tmc5160.TMC5160,
 }
 
@@ -32,23 +32,22 @@ def load_driver(stepper_config):
     driver_name = stepper_config.get('driver', None)
     if driver_name:
         section_name = 'driver %s' % driver_name
-        if stepper_config.has_section(section_name):
-            driver_config = stepper_config.getsection(section_name)
-            section = driver_config.get_name()
-            _type = driver_config.get('type', None)
-            if _type is None:
-                if '2130' in section:
-                    _type = 'TMC2130'
-                elif '2208' in section or '2224' in section:
-                    _type = 'TMC2208'
-                elif '2660' in section:
-                    _type = 'TMC2660'
-                elif '5130' in section:
-                    _type = 'TMC5130'
-                elif '5160' in section:
-                    _type = 'TMC5160'
-            else:
-                _type = _type.upper()
+        driver_config = stepper_config.getsection(section_name)
+        section = driver_config.get_name()
+        _type = driver_config.get('type', None)
+        if _type is None:
+            if '2130' in section:
+                _type = 'TMC2130'
+            elif '2208' in section or '2224' in section:
+                _type = 'TMC2208'
+            elif '2660' in section:
+                _type = 'TMC2660'
+            elif '5130' in section:
+                _type = 'TMC5130'
+            elif '5160' in section:
+                _type = 'TMC5160'
+        else:
+            _type = _type.upper()
     func = DRV_MAPPING.get(_type, driverbase.DriverBase)
     driver = func(driver_config, stepper_config)
     if section:
