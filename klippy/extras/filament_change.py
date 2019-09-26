c051c712d3b27c0b764d9371d8793ae54ecdef7c
cruwaller <cruwaller@gmail.com>
2019-09-26 16:22:40 +0300

    filament_change: continue button support

--- klippy/extras/filament_change.py
+++ klippy/extras/filament_change.py
@@ -4,20 +4,45 @@
 
 class GCodeFilamentPause(object):
     def __init__(self, config):
-        self.printer = printer = config.get_printer()
+        self.printer = config.get_printer()
+        self.reactor = self.printer.get_reactor()
         self.printer.register_event_handler("klippy:ready", self._handle_ready)
-        self.gcode = printer.lookup_object('gcode')
+        self.gcode = self.printer.lookup_object('gcode')
         self.v_sd = None
         self.is_paused = False
         self.resume_print_original = None
+        self.start_time = self.reactor.NEVER
+        self.last_button_state = False
+        self.last_cb_event_time = 0.
+        self.event_delay = config.getfloat('event_delay', 3., above=0.)
         gcode_macro = self.printer.try_load_module(config, 'gcode_macro')
         self.cmd_unload = gcode_macro.load_template(config, 'unload_gcode')
         self.cmd_load = gcode_macro.load_template(config, 'load_gcode')
         self.recover_velocity = config.getfloat('recover_velocity', 40.) * 60.
         self.gcode.register_command('FILAMENT_CHANGE', self.cmd_FILAMENT_CHANGE,
                                     desc="Initiate filament change procedure")
+        switch_pin = config.get("pin", None)
+        if switch_pin is not None:
+            # setup gpio pin to trigger filament change
+            self.buttons = self.printer.try_load_module(config, 'buttons')
+            self.buttons.register_buttons([switch_pin], self._button_handler)
     def _handle_ready(self):
         self.v_sd = self.printer.lookup_object('virtual_sdcard', None)
+        self.start_time = self.reactor.monotonic() + 2.
+    def _button_handler(self, eventtime, state):
+        if eventtime < self.start_time or state == self.last_button_state:
+            self.last_button_state = state
+            return
+        if (eventtime - self.last_cb_event_time) > self.event_delay:
+            self.last_cb_event_time = eventtime
+            if state:
+                # runout detected
+                self.reactor.register_callback(self._ready_event_handler)
+            #else:
+            #    pass
+        self.last_button_state = state
+    def _ready_event_handler(self, _):
+        self.cmd_FILAMENT_CHANGE_READY({})
     def _swap_start_gcode(self):
         if self.resume_print_original is None:
             self.resume_print_original = self.gcode.get_command_handler('M24')
