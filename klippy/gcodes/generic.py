a235584fec32a7bc5a94ba5b12e9713b8730cbdb
cruwaller <cruwaller@gmail.com>
2019-09-27 17:02:56 +0300

    gcode generic: some clean up

--- klippy/gcodes/generic.py
+++ klippy/gcodes/generic.py
@@ -1,37 +1,37 @@
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+#
 
 class GenericGcode:
     def __init__(self, config):
-        self.printer = printer = config.get_printer()
-        self.gcode = gcode = printer.lookup_object('gcode')
-        for cmd in ['M1', 'M118',
-            'M301', 'M302', 'M304',
-            'M851', 'M900']:
+        self.printer = config.get_printer()
+        gcode = self.printer.lookup_object('gcode')
+        for cmd in ['M1', 'M302', 'M851']:
+            wnr = getattr(self, 'cmd_' + cmd + '_when_not_ready', False)
             gcode.register_command(
                 cmd, getattr(self, 'cmd_' + cmd),
+                when_not_ready=wnr,
                 desc=getattr(self, 'cmd_%s_help' % cmd, None))
-        # just discard
+        # Discard not used gcodes:
         for cmd in ['M120', 'M121', 'M122',
             'M291', 'M292',
             'M752', 'M753', 'M754', 'M755', 'M756', 'M997']:
-            gcode.register_command(cmd, gcode.cmd_IGNORE)
+            gcode.register_command(cmd, gcode.cmd_IGNORE, when_not_ready=True)
+        # M118 to ECHO
+        gcode.register_command('M118', gcode.cmd_ECHO, when_not_ready = True,
+                               desc = "Alias to ECHO")
         # M999 to reset
         gcode.register_command('M999', gcode.cmd_FIRMWARE_RESTART,
-                               when_not_ready=True,
-                               desc="Alias to FIRMWARE_RESTART")
-
-    def cmd_ignore(self, params):
-        pass
-
+                               when_not_ready = True,
+                               desc = "Alias to FIRMWARE_RESTART")
     def cmd_M1(self, params):
         toolhead = self.printer.lookup_object('toolhead')
         # Wait for current moves to finish
         toolhead.wait_moves()
         toolhead.motor_heater_off()
 
-    def cmd_M118(self, params):
-        self.gcode.respond(params['#original'].replace(params['#command'], ""))
-
     cmd_M302_help = "Cold Extrude. Args [P<bool>] [S<temp>]"
+    cmd_M302_when_not_ready = True
     def cmd_M302(self, params):
         # Cold extrusion
         #       M302         ; report current cold extrusion state
@@ -40,12 +40,13 @@ class GenericGcode:
         #       M302 S0      ; always allow extrusion (disables checking)
         #       M302 S170    ; only allow extrusion above 170
         #       M302 S170 P1 ; set min extrude temp to 170 but leave disabled
+        gcode = self.printer.lookup_object('gcode')
         disable = None
         temperature = None
         if 'P' in params:
-            disable = self.gcode.get_int('P', params, 0) == 1
+            disable = gcode.get_int('P', params, 0) == 1
         if 'S' in params:
-            temperature = self.gcode.get_int('S', params, -1)
+            temperature = gcode.get_int('S', params, -1)
         resp = []
         for extruder in self.printer.extruder_get().values():
             heater = extruder.get_heater()
@@ -53,31 +54,37 @@ class GenericGcode:
             status, temp = heater.get_min_extrude_status()
             resp.append("Heater '%s' cold extrude: %s, min temp %.2fC"
                         % (heater.name, status, temp))
-        self.gcode.respond("\n".join(resp))
-
-    def cmd_M301(self, params):
-        # M301: Set PID parameters
-        self.gcode.respond("Obsolete, use SET_PID_PARAMS")
-
-    def cmd_M304(self, params):
-        # M304: Set PID parameters - Bed
-        self.gcode.respond("Obsolete, use SET_PID_PARAMS")
+        gcode.respond("\n".join(resp))
 
     cmd_M851_help = "Set axis offset. Args [X<offset] [Y<offset>] [Z<offset>]"
+    cmd_M851_when_not_ready = True
     def cmd_M851(self, params):
+        gcode = self.printer.lookup_object('gcode')
         toolhead = self.printer.lookup_object('toolhead')
-        axis2pos = self.gcode.axis2pos
+        axis2pos = gcode.axis2pos
         # Set X, Y, Z offsets
         rails = toolhead.get_kinematics().get_rails()
-        offsets = {axis2pos[a]: self.gcode.get_float(a, params)
+        offsets = {axis2pos[a]: gcode.get_float(a, params)
             for a, p in axis2pos.items() if a in params}
         for p, offset in offsets.items():
             rails[p].set_homing_offset(offset)
-        self.gcode.respond("Current offsets: X=%.2f Y=%.2f Z=%.2f" %
-                           (rails[0].homing_offset,
-                           rails[1].homing_offset,
-                           rails[2].homing_offset))
+        gcode.respond("Current offsets: X=%.2f Y=%.2f Z=%.2f" %
+                      (rails[0].homing_offset,
+                       rails[1].homing_offset,
+                       rails[2].homing_offset))
+    def cmd_M851_new_way(self, params):
+        gcode = self.printer.lookup_object('gcode')
+        cmd = []
+        for axis in "XYZ":
+            if axis in params:
+                cmd.append("%s_ADJUST=%f" % (
+                    axis, gcode.get_float(axis, params)))
+        if cmd:
+            gcode.run_script_from_command(
+                "SET_GCODE_OFFSET %s\n" % " ".join(cmd))
+        # print out the current position + offsets
+        gcode.run_script_from_command("GET_POSITION\n")
 
-    def cmd_M900(self, params):
-        # Pressure Advance configuration
-        self.gcode.respond_error("Obsolete, use SET_PRESSURE_ADVANCE")
+
+#def load_config(config):
+#    return GenericGcode(config)
