1449136a10d8d19c0bfcc6e28b6109cada36fa64
cruwaller <cruwaller@gmail.com>
2019-12-04 10:25:49 +0200

    tmc: has_reg changed to has_register

--- klippy/extras/tmc.py
+++ klippy/extras/tmc.py
@@ -27,7 +27,7 @@ class FieldHelper:
                                    for f in fields }
     def lookup_register(self, field_name, default=None):
         return self.field_to_register.get(field_name, default)
-    def has_reg(self, reg_name):
+    def has_register(self, reg_name):
         return reg_name in self.all_fields
     def set_reg_value(self, reg_name, new_value):
         self.registers[reg_name] = new_value
@@ -108,7 +108,7 @@ class TMCCommandHelper:
         # Send registers
         for reg_name, val in self.fields.registers.items():
             self.mcu_tmc.set_register(reg_name, val, print_time)
-        if self.fields.has_reg("GSTAT"):
+        if self.fields.has_register("GSTAT"):
             self.mcu_tmc.set_register("GSTAT", 7) # reset status
     def _handle_connect(self):
         # Check for soft stepper enable/disable
@@ -184,7 +184,7 @@ class TMCCommandHelper:
             if self.read_translate is not None:
                 reg_name, val = self.read_translate(reg_name, val)
             self.gcode.respond_info(self.fields.pretty_format(reg_name, val))
-        if self.fields.has_reg("GSTAT"):
+        if self.fields.has_register("GSTAT"):
             self.mcu_tmc.set_register("GSTAT", 7) # reset status
 
 
